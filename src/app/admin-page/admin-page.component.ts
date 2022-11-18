55112441a35d34962f55f7f68ed3401cd81fe92a
BehzodRj <behzodfarishta@gmail.com>
2022-11-18 18:04:17 +0500

    worked with todo list

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as Chart from 'chart.js';
 import * as $ from 'jquery'
 import { Router } from '@angular/router';
+import { RequestsService } from '../all.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -9,13 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent implements OnInit {
+  recentSalesData: any = []
+  todoData: any = []
   chartWorldWideSales: any;
   chartSalesRevenue: any;
+  labels: any = []
+  data: any = []
   dateInline: any
   userData: any = localStorage.getItem('access_token')
-  todoData: any = []
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private request: RequestsService) { }
 
   ngOnInit() {
     setTimeout(() => {
@@ -23,24 +27,30 @@ export class AdminPageComponent implements OnInit {
       spinner?.classList.remove('show')
     }, 600);
 
+    this.recentSalesData = this.request.reventCales()
+    this.recentSalesData.forEach( (elem: any) => {
+      this.labels.push(new Date(elem.date).getFullYear())
+      this.data.push(elem.amount)
+    })
+
     // World Wide Sales Chart
     this.chartWorldWideSales = new Chart('worldWideSales', {
       type: "bar",
       data: {
-        labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+        labels: this.labels,
         datasets: [{
           label: "USA",
-          data: [15, 30, 55, 65, 60, 80, 95],
+          data: this.data,
           backgroundColor: "rgba(235, 22, 22, .7)"
         },
         {
           label: "UK",
-          data: [8, 35, 40, 60, 70, 55, 75],
+          data: this.data,
           backgroundColor: "rgba(235, 22, 22, .5)"
         },
         {
           label: "AU",
-          data: [12, 25, 45, 55, 65, 70, 60],
+          data: this.data,
           backgroundColor: "rgba(235, 22, 22, .3)"
         }
         ]
@@ -54,16 +64,16 @@ export class AdminPageComponent implements OnInit {
     this.chartSalesRevenue = new Chart('salesRevenue', {
       type: "line",
       data: {
-        labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+        labels: this.labels,
         datasets: [{
           label: "Salse",
-          data: [15, 30, 55, 45, 70, 65, 85],
+          data: this.data,
           backgroundColor: "rgba(235, 22, 22, .7)",
           fill: true
         },
         {
           label: "Revenue",
-          data: [99, 135, 170, 130, 190, 180, 270],
+          data: this.data,
           backgroundColor: "rgba(235, 22, 22, .5)",
           fill: true
         }
--- src/app/all.service.ts
+++ src/app/all.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class RequestsService {
 
   constructor() { }
+
+
+  reventCales() {
+    return [
+      {date: new Date().toISOString(), invoice: 'INV-0123', customer: 'Behzod Rajabaliev', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0124', customer: 'Vafo Boqiev', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0125', customer: 'Hasanzoda Muhammad', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0126', customer: 'Jake Pratt', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0127', customer: 'Manuchehr Shamolov', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0128', customer: 'Nekruz Sattorov', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0129', customer: 'Munis Umedzoda', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0130', customer: 'Samar Salimov', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0131', customer: 'Jahongir Davlatov', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0132', customer: 'Rustam Boboev', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0133', customer: 'Ozod Davronov', amount: Math.round(Math.random() * 150), status: 'Paid'},
+      {date: new Date().toISOString(), invoice: 'INV-0134', customer: 'Davron Sultonov', amount: Math.round(Math.random() * 150), status: 'Paid'}
+    ]
+  }
+
 }
